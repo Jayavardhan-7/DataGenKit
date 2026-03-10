491e56c451361514b0b31ea7acf05514a5f6b894
jayavardhan perala <jayavardhanperala@gmail.com>
2026-03-10 19:02:10 +0530

    Initial commit: Core Datagenkit Python lib

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from setuptools import setup, find_packages
+import os
+
+with open(os.path.join(os.path.dirname(__file__), "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
 
 setup(
     name="Datagenkit",
     version="0.2.0",
     description="A local Image Dataset Generator using smart augmentations and MobileNetV2 filtering.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Jayavardhan",
     packages=find_packages(),
     install_requires=[
